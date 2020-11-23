6bcadc59df388c4f537ca331fc75194a58e9c1d7
Dmitry Litovchenko <Dmitry.Litovchenko1@yandex.ru>
2020-11-23 14:35:01 +0300

    Fix semicolon insertion in lexer (Go)

--- golang/CSharp/GoLexerBase.cs
+++ golang/CSharp/GoLexerBase.cs
@@ -11,6 +11,7 @@ namespace GoParseTree
             IDENTIFIER, R_PAREN, R_CURLY, R_BRACKET, INTERPRETED_STRING_LIT, RAW_STRING_LIT, PLUS_PLUS, MINUS_MINUS,
             DECIMAL_LIT, IMAGINARY_LIT, RUNE_LIT, FLOAT_LIT, BREAK, FALLTHROUGH, CONTINUE, RETURN
         };
+        private readonly HashSet<int> symbolsToSkip = new HashSet<int> { '\t', ' ', '\r'};
 
         protected GoLexerBase(ICharStream input) : base(input)
         {
@@ -18,13 +19,25 @@ namespace GoParseTree
 
         public override IToken NextToken()
         {
-            if (tokensToEmmitSemicolon.Contains(Type) && _input.La(2) == '\n')
+            
+            if (tokensToEmmitSemicolon.Contains(Type))
             {
-                IToken token = _factory.Create(_tokenFactorySourcePair, SEMI, "", _channel, _tokenStartCharIndex,
-                    CharIndex - 1, _tokenStartLine, _tokenStartCharPositionInLine);
-                Emit(token);
-                Type = token.Type;
-                return token;
+                var index = 1;
+                int next;
+                do
+                {
+                    next = _input.La(index);
+                    index++;
+                } while (symbolsToSkip.Contains(next));
+
+                if (next == '\n' || (next == '/' && _input.La(index) == '/'))
+                {
+                    IToken token = _factory.Create(_tokenFactorySourcePair, SEMI, "", _channel, _tokenStartCharIndex,
+                        CharIndex - 1, _tokenStartLine, _tokenStartCharPositionInLine);
+                    Emit(token);
+                    Type = token.Type;
+                    return token;
+                }
             }
 
             return base.NextToken();
